e9f323687ebbe8568d69e22a1a1374820f915a71
WaGaCAD <waldek@parikon.pl>
2018-12-01 17:50:04 +0100

    20181130

---
+++ proj_forum/Base.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SQLite;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace proj_forum
+{
+    class Base
+    {
+        // zmienna path nie jest potrzebna w zadnej z metod
+        // można ją spokojnie przenieść do konstruktora jako zmienną prywatną
+        // ale może się przyda więc zostaje
+        private string path;
+        // do połączenia wystarczy db_con ustawiony raz w konstruktorze
+        private SQLiteConnection db_con;
+
+
+        /// <summary>
+        /// Konstruktor klasy. Pobiera ścieżkę do bazy.
+        /// </summary>
+        /// <param name="_path"></param>
+
+        public Base(string _path)
+        {
+            this.path = _path + "\\Bazy";
+            string sciezkaBaza = this.path + "\\projekty.sqlite";
+            this.db_con = new SQLiteConnection($"Data Source = {sciezkaBaza}; Version = 3");
+        }
+
+        public interface IBase
+        {
+            bool AddBaseIfNotExist();
+            int GetBaseVersion();
+        }
+        /// <summary>
+        /// Tworzy bazę jeśli nie istnieje i tabelę projekt_lista
+        /// </summary>
+        public void AddBaseIfNotExist()
+        {
+
+            if (!Directory.Exists(this.path))
+            {
+                Directory.CreateDirectory(this.path);
+            }
+            this.db_con.Open();
+            SQLiteCommand db_cmd = db_con.CreateCommand();
+            db_cmd.CommandType = System.Data.CommandType.Text;
+            db_cmd.CommandText = "CREATE TABLE IF NOT EXISTS projekt_lista ([Numer projektu]  INT, [Nazwa projektu]  TEXT, [Data dodania] TEXT, [Data zakończenia] TEXT, [Ścieżka] TEXT)";
+            db_cmd.ExecuteNonQuery();
+            db_cmd.CommandText = "CREATE TABLE IF NOT EXISTS wersja_bazy ([Numer wersji]  INT)";
+            db_cmd.ExecuteNonQuery();
+            this.db_con.Close();
+        }
+
+        public int GetBaseVersion()
+
+        {
+            int wersja = 1;
+
+            this.db_con.Open();
+            SQLiteCommand db_cmd = db_con.CreateCommand();
+            db_cmd.CommandType = CommandType.Text;
+            db_cmd.CommandText = "select * from wersja_bazy";
+            db_cmd.ExecuteNonQuery();
+            var dt = new DataTable();
+            SQLiteDataAdapter da = new SQLiteDataAdapter(db_cmd);
+            da.Fill(dt);
+            int wierszy = dt.Rows.Count;
+            if (wierszy == 0)
+            {
+                db_cmd.CommandText = "INSERT INTO wersja_bazy VALUES ('" + wersja + "')";
+                db_cmd.ExecuteNonQuery();
+                this.db_con.Close();
+            }
+            else
+            {
+                this.db_con.Close();
+                DataRow dtr = dt.Rows[0];
+                wersja = Convert.ToInt16(dtr[0]);
+            }
+
+            return wersja; // metoda zwróci wersję
+        }
+
+        public DataTable PobierzDaneTabeli(string nazwa_tabeli)
+        {
+            var dt = new DataTable();
+            this.db_con.Open();
+            SQLiteCommand db_cmd = db_con.CreateCommand();
+            db_cmd.CommandType = CommandType.Text;
+            db_cmd.CommandText = "select * from " + nazwa_tabeli + "";
+            db_cmd.ExecuteNonQuery();
+            SQLiteDataAdapter da = new SQLiteDataAdapter(db_cmd);
+            da.Fill(dt);
+            this.db_con.Close();
+            return dt;
+        }
+
+    }
+}
--- proj_forum/MainWindow.xaml.cs
+++ proj_forum/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Windows;
@@ -13,15 +14,45 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace proj_forum
-{
-    /// <summary>
-    /// Logika interakcji dla klasy MainWindow.xaml
-    /// </summary>
+{/// <summary>
+ /// Logika interakcji dla klasy MainWindow.xaml
+ /// </summary>
     public partial class MainWindow : Window
     {
+        string path;
+
         public MainWindow()
         {
             InitializeComponent();
+            //zamykamy pewne funkcjonalności, gdyż musielibyśmy inaczej oprogramować obiekt DataGrid, a tego nie potrafię
+            datagrid_projekt_lista.CanUserAddRows = false;
+            datagrid_projekt_lista.CanUserDeleteRows = false;
+            datagrid_projekt_lista.IsReadOnly = true;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                //path = (new System.Uri(Assembly.GetExecutingAssembly().CodeBase)).AbsolutePath;
+                path = System.IO.Path.Combine(System.IO.Path.GetDirectoryName(this.GetType().Assembly.Location));
+                var baza = new Base(path);
+                baza.AddBaseIfNotExist();
+                if (baza.GetBaseVersion() != 1)
+                {
+                    // tutaj dałbym konwerter na starej bazy na nową, ale narazie nie mamy takiego konwertera więc program tego nie zrobi
+                    MessageBox.Show("Nieprawidłowa wersja bazy. Konwersja niemożliwa w tej wersji programu.Nastąpi zamknięcie programu", "Projekty-info");
+                    this.Close();
+                }
+                DataTable tablicaprojektow = baza.PobierzDaneTabeli("projekt_lista");
+                datagrid_projekt_lista.ItemsSource = tablicaprojektow.DefaultView;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message + ". Program zostanie zamknięty.", "Projekty-info");
+                this.Close();
+            }
+
         }
     }
 }
